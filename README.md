fdd27aeff4618613c6d18b4c0ff46c37e8e4f8fb
Jimmi Dyson <jimmidyson@gmail.com>
2015-04-10 12:06:39 +0100

    Add in service DNS option - no API server calls required\!

--- README.md
+++ README.md
@@ -23,7 +23,7 @@ Here is a simple sample configuration:
 ```yaml
 cloud:
   k8s:
-    selector: ${SELECTOR}
+    servicedns: ${SERVICE_DNS}
 discovery:
     type: io.fabric8.elasticsearch.discovery.k8s.K8sDiscoveryModule
 
@@ -34,4 +34,227 @@ path:
   work: /data/work
 ```
 
-You can then set the `SELECTOR` environment variable to specify the selector for your Elasticsearch pods, e.g. `cluster=myCluster,component=elasticsearch`.
+If you want to use a Kubernetes pod selector instead, then use `selector: ${SELECTOR}` instead of the `serviceDns` setting.
+
+Depending on which once you choose, you can then either:
+
+* Set the `SELECTOR` environment variable to specify the selector for your Elasticsearch pods, e.g. `cluster=myCluster,component=elasticsearch`.
+* Set the `SERVICE_DNS` environment variable to specify the service name to look up endpoints for in DNS, e.g. `elasticsearch-cluster`.
+
+# Kubernetes example
+
+In this example, we're going to use `servicedns` to look up the Elasticsearch cluster nodes to join.
+
+```json
+{
+  "id": "elasticsearch-list",
+  "kind": "List",
+  "apiVersion": "v1beta2",
+  "name": "elasticsearch-config",
+  "description": "Configuration for elasticsearch",
+  "items": [
+    {
+      "id": "elasticsearch",
+      "apiVersion": "v1beta1",
+      "kind": "Service",
+      "containerPort": 9200,
+      "port": 9200,
+      "selector": {
+        "component": "elasticsearch",
+        "type": "client",
+        "provider": "fabric8"
+      }
+    },
+    {
+      "id": "elasticsearch-cluster",
+      "apiVersion": "v1beta1",
+      "PortalIP": "None",
+      "kind": "Service",
+      "containerPort": 9300,
+      "port": 9300,
+      "selector": {
+        "component": "elasticsearch",
+        "provider": "fabric8"
+      }
+    },
+    {
+      "id": "elasticsearch-client-rc",
+      "kind": "ReplicationController",
+      "apiVersion": "v1beta1",
+      "desiredState": {
+        "replicas": 1,
+        "replicaSelector": {
+          "component": "elasticsearch",
+          "type": "client",
+          "provider": "fabric8"
+        },
+        "podTemplate": {
+          "desiredState": {
+            "manifest": {
+              "version": "v1beta1",
+              "id": "elasticsearchPod",
+              "containers": [
+                {
+                  "name": "elasticsearch-container",
+                  "image": "fabric8/elasticsearch-k8s:1.5.0",
+                  "imagePullPolicy": "PullIfNotPresent",
+                  "env": [
+                    {
+                      "name": "SERVICE_DNS",
+                      "value": "elasticsearch-cluster"
+                    },
+                    {
+                      "name": "KUBERNETES_TRUST_CERT",
+                      "value": "true"
+                    },
+                    {
+                      "name": "NODE_DATA",
+                      "value": "false"
+                    },
+                    {
+                      "name": "NODE_MASTER",
+                      "value": "false"
+                    }
+                  ],
+                  "ports": [
+                    {
+                      "containerPort": 9200
+                    },
+                    {
+                      "containerPort": 9300
+                    }
+                  ]
+                }
+              ]
+            }
+          },
+          "labels": {
+            "component": "elasticsearch",
+            "type": "client",
+            "provider": "fabric8"
+          }
+        }
+      },
+      "labels": {
+        "component": "elasticsearch",
+        "type": "client",
+        "provider": "fabric8"
+      }
+    },
+    {
+      "id": "elasticsearch-data-rc",
+      "kind": "ReplicationController",
+      "apiVersion": "v1beta1",
+      "desiredState": {
+        "replicas": 1,
+        "replicaSelector": {
+          "component": "elasticsearch",
+          "type": "data",
+          "provider": "fabric8"
+        },
+        "podTemplate": {
+          "desiredState": {
+            "manifest": {
+              "version": "v1beta1",
+              "id": "elasticsearchPod",
+              "containers": [
+                {
+                  "name": "elasticsearch-container",
+                  "image": "fabric8/elasticsearch-k8s:1.5.0",
+                  "imagePullPolicy": "PullIfNotPresent",
+                  "env": [
+                    {
+                      "name": "SERVICE_DNS",
+                      "value": "elasticsearch-cluster"
+                    },
+                    {
+                      "name": "KUBERNETES_TRUST_CERT",
+                      "value": "true"
+                    },
+                    {
+                      "name": "NODE_MASTER",
+                      "value": "false"
+                    }
+                  ],
+                  "ports": [
+                    {
+                      "containerPort": 9300
+                    }
+                  ]
+                }
+              ]
+            }
+          },
+          "labels": {
+            "component": "elasticsearch",
+            "type": "data",
+            "provider": "fabric8"
+          }
+        }
+      },
+      "labels": {
+        "component": "elasticsearch",
+        "type": "data",
+        "provider": "fabric8"
+      }
+    },
+    {
+      "id": "elasticsearch-master-rc",
+      "kind": "ReplicationController",
+      "apiVersion": "v1beta1",
+      "desiredState": {
+        "replicas": 1,
+        "replicaSelector": {
+          "component": "elasticsearch",
+          "type": "master",
+          "provider": "fabric8"
+        },
+        "podTemplate": {
+          "desiredState": {
+            "manifest": {
+              "version": "v1beta1",
+              "id": "elasticsearchPod",
+              "containers": [
+                {
+                  "name": "elasticsearch-container",
+                  "image": "fabric8/elasticsearch-k8s:1.5.0",
+                  "imagePullPolicy": "PullIfNotPresent",
+                  "env": [
+                    {
+                      "name": "SERVICE_DNS",
+                      "value": "elasticsearch-cluster"
+                    },
+                    {
+                      "name": "KUBERNETES_TRUST_CERT",
+                      "value": "true"
+                    },
+                    {
+                      "name": "NODE_DATA",
+                      "value": "false"
+                    }
+                  ],
+                  "ports": [
+                    {
+                      "containerPort": 9300
+                    }
+                  ]
+                }
+              ]
+            }
+          },
+          "labels": {
+            "component": "elasticsearch",
+            "type": "master",
+            "provider": "fabric8"
+          }
+        }
+      },
+      "labels": {
+        "component": "elasticsearch",
+        "type": "master",
+        "provider": "fabric8"
+      }
+    }
+  ]
+}
+```
--- src/main/java/io/fabric8/elasticsearch/discovery/k8s/K8sUnicastHostsProvider.java
+++ src/main/java/io/fabric8/elasticsearch/discovery/k8s/K8sUnicastHostsProvider.java
@@ -24,6 +24,7 @@ import java.net.InetAddress;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class K8sUnicastHostsProvider extends AbstractComponent implements UnicastHostsProvider {
 
@@ -35,14 +36,16 @@ public class K8sUnicastHostsProvider extends AbstractComponent implements Unicas
         public static final String REFRESH = "refresh_interval";
         public static final String VERSION = "Elasticsearch/K8sCloud/1.0";
         public static final String SELECTOR = "selector";
+        public static final String SERVICE_DNS = "servicedns";
     }
 
-    private Kubernetes kubernetes = new KubernetesFactory().createKubernetes();
+    private Kubernetes kubernetes;
 
     private TransportService transportService;
     private NetworkService networkService;
 
     private final String selector;
+    private final String serviceDns;
 
     private final TimeValue refreshInterval;
     private long lastRefresh;
@@ -60,9 +63,12 @@ public class K8sUnicastHostsProvider extends AbstractComponent implements Unicas
                 settings.getAsTime("cloud.k8s." + Fields.REFRESH, TimeValue.timeValueSeconds(0)));
 
         this.selector = componentSettings.get(Fields.SELECTOR, settings.get("cloud.k8s." + Fields.SELECTOR));
+        this.serviceDns = componentSettings.get(Fields.SELECTOR, settings.get("cloud.k8s." + Fields.SERVICE_DNS));
 
         // Check that we have all needed properties
-        checkProperty(Fields.SELECTOR, selector);
+        if (!(Strings.hasText(this.selector) || Strings.hasText(this.serviceDns))) {
+            logger.warn("Neither cloud.k8s.{} or cloud.k8s.{} are set.", Fields.SELECTOR, Fields.SERVICE_DNS);
+        }
     }
 
     /**
@@ -84,11 +90,11 @@ public class K8sUnicastHostsProvider extends AbstractComponent implements Unicas
         logger.debug("start building nodes list using Kubernetes API");
 
         cachedDiscoNodes = Lists.newArrayList();
-        String ipAddress = null;
+        String currentIpAddress = null;
         try {
             InetAddress inetAddress = networkService.resolvePublishHostAddress(null);
             if (inetAddress != null) {
-                ipAddress = inetAddress.getHostAddress();
+                currentIpAddress = inetAddress.getHostAddress();
             }
         } catch (IOException e) {
             // We can't find the publish host address... Hmmm. Too bad :-(
@@ -96,54 +102,10 @@ public class K8sUnicastHostsProvider extends AbstractComponent implements Unicas
         }
 
         try {
-            Map<String, Pod> podMap = KubernetesHelper.getSelectedPodMap(kubernetes, selector);
-            Collection<Pod> pods = podMap.values();
-
-            if (pods == null) {
-                logger.trace("no pod found for selector [{}].", this.selector);
-                return cachedDiscoNodes;
-            }
-
-            for (Pod pod : pods) {
-                String status = pod.getCurrentState().getStatus();
-                logger.trace("k8s instance {} with status {} found.", pod.getId(), status);
-
-                if (!status.equals(Status.RUNNING)) {
-                    logger.trace("k8s instance {} is not running - ignoring.", pod.getId());
-                    continue;
-                }
-
-                try {
-                    String podIp = pod.getCurrentState().getPodIP();
-
-                    if (podIp.equals(ipAddress)) {
-                        // We found the current node.
-                        // We can ignore it in the list of DiscoveryNode
-                        logger.trace("current node found. Ignoring {} - {}", pod.getId(), podIp);
-                    } else {
-                        List<Container> containers = KubernetesHelper.getContainers(pod);
-                        for (Container container : containers) {
-                            logger.trace("pod " + pod.getId() + " container: " + container.getName() + " image: " + container.getImage());
-                            List<Port> ports = container.getPorts();
-                            for (Port port : ports) {
-                                Integer containerPort = port.getContainerPort();
-                                if (containerPort == 9300) {
-                                    String address = podIp.concat(":").concat(containerPort .toString());
-
-                                    // pod IP is a single IP Address. We need to build a TransportAddress from it
-                                    TransportAddress[] addresses = transportService.addressesFromString(address);
-
-                                    logger.trace("adding {}, address {}, transport_address {}, status {}", pod.getId(),
-                                        podIp, addresses[0], status);
-                                    cachedDiscoNodes.add(new DiscoveryNode("#cloud-" + pod.getId() + "-" + 0, addresses[0], Version.CURRENT));
-                                }
-                            }
-                        }
-                    }
-                } catch (Exception e) {
-                    logger.warn("failed to add {}", e, pod.getId());
-                }
-
+            if (Strings.hasText(this.selector)) {
+                return getNodesFromKubernetesSelector(currentIpAddress);
+            } else if (Strings.hasText(this.serviceDns)) {
+                return getNodesFromKubernetesServiceDns(currentIpAddress);
             }
         } catch (Throwable e) {
             logger.warn("Exception caught during discovery {} : {}", e, e.getClass().getName(), e.getMessage());
@@ -156,9 +118,91 @@ public class K8sUnicastHostsProvider extends AbstractComponent implements Unicas
         return cachedDiscoNodes;
     }
 
-    private void checkProperty(String name, String value) {
-        if (!Strings.hasText(value)) {
-            logger.warn("cloud.k8s.{} is not set.", name);
+    private List<DiscoveryNode> getNodesFromKubernetesSelector(String currentIpAddress) {
+        Map<String, Pod> podMap = KubernetesHelper.getSelectedPodMap(getKubernetes(), selector);
+        Collection<Pod> pods = podMap.values();
+
+        if (pods == null) {
+            logger.trace("no pod found for selector [{}].", this.selector);
+            return cachedDiscoNodes;
         }
+
+        for (Pod pod : pods) {
+            String status = pod.getCurrentState().getStatus();
+            logger.trace("k8s instance {} with status {} found.", pod.getId(), status);
+
+            if (!status.equals(Status.RUNNING)) {
+                logger.trace("k8s instance {} is not running - ignoring.", pod.getId());
+                continue;
+            }
+
+            try {
+                String podIp = pod.getCurrentState().getPodIP();
+
+                if (podIp.equals(currentIpAddress)) {
+                    // We found the current node.
+                    // We can ignore it in the list of DiscoveryNode
+                    logger.trace("current node found. Ignoring {} - {}", pod.getId(), podIp);
+                } else {
+                    List<Container> containers = KubernetesHelper.getContainers(pod);
+                    for (Container container : containers) {
+                        logger.trace("pod " + pod.getId() + " container: " + container.getName() + " image: " + container.getImage());
+                        List<Port> ports = container.getPorts();
+                        for (Port port : ports) {
+                            Integer containerPort = port.getContainerPort();
+                            if (containerPort == 9300) {
+                                String address = podIp.concat(":").concat(containerPort.toString());
+
+                                // pod IP is a single IP Address. We need to build a TransportAddress from it
+                                TransportAddress[] addresses = transportService.addressesFromString(address);
+
+                                logger.trace("adding {}, address {}, transport_address {}, status {}", pod.getId(),
+                                        podIp, addresses[0], status);
+                                cachedDiscoNodes.add(new DiscoveryNode("#cloud-" + pod.getId() + "-" + 0, addresses[0], Version.CURRENT));
+                            }
+                        }
+                    }
+                }
+            } catch (Exception e) {
+                logger.warn("failed to add {}", e, pod.getId());
+            }
+        }
+
+        return cachedDiscoNodes;
+    }
+
+    private List<DiscoveryNode> getNodesFromKubernetesServiceDns(String currentIpAddress) throws Exception {
+        Set<String> serivceEndpointIps = KubernetesHelper.lookupServiceInDns(serviceDns);
+
+        if (serivceEndpointIps == null) {
+            logger.trace("no service endpoints found for service name [{}].", this.serviceDns);
+            return cachedDiscoNodes;
+        }
+
+        for (String endpointIp : serivceEndpointIps) {
+            if (endpointIp.equals(currentIpAddress)) {
+                // We found the current node.
+                // We can ignore it in the list of DiscoveryNode
+                logger.trace("current node found. Ignoring {} - {}", endpointIp);
+            } else {
+                String address = endpointIp.concat(":9300");
+
+                // pod IP is a single IP Address. We need to build a TransportAddress from it
+                TransportAddress[] addresses = transportService.addressesFromString(address);
+
+                logger.trace("adding address {}, transport_address {}",
+                        endpointIp, addresses[0]);
+                cachedDiscoNodes.add(new DiscoveryNode("#cloud-".concat(serviceDns).concat("-").concat(endpointIp) + "-" + 0, addresses[0], Version.CURRENT));
+            }
+        }
+
+        return cachedDiscoNodes;
+    }
+
+    private Kubernetes getKubernetes() {
+        if (kubernetes == null) {
+            kubernetes = new KubernetesFactory().createKubernetes();
+        }
+        return kubernetes;
     }
 }
