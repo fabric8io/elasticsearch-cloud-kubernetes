14ccd226721296bbfcf4d41fbdc3fdabcf261000
Jimmi Dyson <jimmidyson@gmail.com>
2015-11-19 13:27:58 +0000

    Fix potential NPE

--- src/main/java/io/fabric8/elasticsearch/cloud/kubernetes/KubernetesAPIServiceImpl.java
+++ src/main/java/io/fabric8/elasticsearch/cloud/kubernetes/KubernetesAPIServiceImpl.java
@@ -1,5 +1,6 @@
 package io.fabric8.elasticsearch.cloud.kubernetes;
 
+import io.fabric8.kubernetes.api.model.Endpoints;
 import io.fabric8.kubernetes.client.DefaultKubernetesClient;
 import io.fabric8.kubernetes.client.KubernetesClient;
 import org.elasticsearch.ElasticsearchException;
@@ -24,16 +25,19 @@ public class KubernetesAPIServiceImpl extends AbstractLifecycleComponent<Kuberne
   public Collection<InetAddress> endpoints() {
     logger.debug("get endpoints for service {}, namespace {}", serviceName, namespace);
     final Set<InetAddress> instances = new HashSet<>();
-    client().endpoints().inNamespace(namespace).withName(serviceName).get().getSubsets().stream().forEach((endpointSubset) -> {
-      endpointSubset.getAddresses().stream().forEach((endpointAddress -> {
-        String ip = endpointAddress.getIp();
-        try {
-          instances.add(InetAddress.getByName(ip));
-        } catch (UnknownHostException e) {
-          logger.warn("Ignoring invalid endpoint IP address: {}", ip);
-        }
-      }));
-    });
+    Endpoints endpoints = client().endpoints().inNamespace(namespace).withName(serviceName).get();
+    if (endpoints != null && endpoints.getSubsets() != null) {
+      endpoints.getSubsets().stream().forEach((endpointSubset) -> {
+        endpointSubset.getAddresses().stream().forEach((endpointAddress -> {
+          String ip = endpointAddress.getIp();
+          try {
+            instances.add(InetAddress.getByName(ip));
+          } catch (UnknownHostException e) {
+            logger.warn("Ignoring invalid endpoint IP address: {}", ip);
+          }
+        }));
+      });
+    }
 
     if (instances.isEmpty()) {
       logger.warn("disabling Kubernetes discovery. Can not get list of endpoints");
